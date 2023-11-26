569a42f921c9964f03905d809b1d99bbc7a0524d
Ilmars Marcins <ilmars@gmail.com>
2023-11-26 21:52:48 +0200

    At start collects neutral value range. And limit PPM angle for ESC to +- 45 deg.

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ from machine import Pin, ADC
 from ppm import Servo
 
 
+def collect_hall_neutral(hall):
+    i = 0
+    steps = 50
+    neutral = 0
+    min_v = 999999
+    max_v = 0
+    while True:
+        i += 1
+        if i > steps:
+            break
+        time.sleep(0.01)
+        value = hall.read_u16()
+        neutral += value / steps
+        if value < min_v:
+            min_v = value
+        if value > max_v:
+            max_v = value
+
+    return neutral, min_v, max_v
+
+
 led = Pin(25, Pin.OUT)
 
 print("Hello world")
@@ -14,6 +35,7 @@ servo = Servo(0, angle)
 hall = ADC(26)
 hall_min = 16200
 hall_max = 51000
+hall_neutral, hall_neutral_min, hall_neutral_max = collect_hall_neutral(hall)
 
 i = 0
 while True:
@@ -25,16 +47,22 @@ while True:
     time.sleep(0.01)
     # HALL read
     hall_value = hall.read_u16()
-    if i % 100 == 0:
-        led.toggle()
-        print("hall_value: %d" % hall_value)
     if hall_value > hall_max:
         hall_value = hall_max
     if hall_value < hall_min:
         hall_value = hall_min
 
-    hall_fraction = (hall_value - hall_min) / (hall_max - hall_min)
-    angle = hall_fraction * 180
+    if hall_value > hall_neutral_max:
+        hall_fraction = (hall_value - hall_neutral_max) / (hall_max - hall_neutral_max)
+        angle = 90 + (hall_fraction * 45)
+    else:
+        hall_fraction = (hall_neutral_min - hall_value) / (hall_neutral_min - hall_min)
+        angle = 90 - (hall_fraction * 45)
+
+    if i % 100 == 0:
+        led.toggle()
+        print("neutral: (%d,%d,%d) hall_value: %d angle: %f" % (hall_neutral, hall_neutral_min, hall_neutral_max, hall_value, angle))
+
     # PPM output
     # angle += angle_add
     # if angle >= 180:
@@ -42,3 +70,5 @@ while True:
     # if angle <= 0:
     #     angle_add = 0 - angle_add
     servo.move(angle)
+
+
